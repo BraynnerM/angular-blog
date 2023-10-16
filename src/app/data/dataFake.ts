2b35d63f1019096cf6a21d56c4d12ebf5961c7c7
Braynner Marques <braynnermarques@hotmail.com>
2023-10-16 14:53:49 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+    "description": "Marvel studios anuncia novo filme do homem de ferro.",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "NOVO FILME DOS X-MEN ESTÁ EM PÓS PRODUÇÃO AFIRMA DIRETOR",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque non explicabo itaque, eveniet sint ipsum maxime molestiae corporis atque, nihil et excepturi voluptas expedita! Repellendus totam debitis ullam ex rerum.",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqHgnkkwF9sGkCKyo-9S3m59jwYJNad6jsZi0RUqSk16Ctq9-0bQ5lBUyYth_5v8DugkA&usqp=CAU"
+  },
+  {
+    "id":"3",
+    "title": "NOVA SÉRIE DA MARVEL STUDIOS EM BREVE NO DISNEY+",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque non explicabo itaque, eveniet sint ipsum maxime molestiae corporis atque, nihil et excepturi voluptas expedita! Repellendus totam debitis ullam ex rerum.",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"4",
+    "title": "NOVO FILME DO PANTERA NEGRA SERÁ LANÇADO EM BREVE",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque non explicabo itaque, eveniet sint ipsum maxime molestiae corporis atque, nihil et excepturi voluptas expedita! Repellendus totam debitis ullam ex rerum.",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F6482D58C8BB4D899FA4682BF27652C3E1BB73F5971E5F963994112F26A73690/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  }
+]
+
+
